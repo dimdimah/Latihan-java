34c97820f8ad80ae9c341d09950d6ae5ec35db7c
dimdimah <dimahluthfi15@gmail.com>
2023-05-31 20:46:54 +0700

    nambah folder utsDimah untuk penilaian uts java 2023

---
+++ Main/src/utsDimah/outputNim.java
@@ -0,0 +1,9 @@
+package utsDimah;
+
+public class outputNim {
+    public static void main(String[] args) {
+        int myNim = 2113010295;
+        String myName = "Dimah Luthfi Aulia Rohman";
+        System.out.println("Hallo nama saya " + myName + " dengan Nomer Induk Mahasiswa " + myNim);
+    }
+}
